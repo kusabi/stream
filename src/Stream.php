5b4531ab91b3d5358cc060d57f4251368e782d22
Christian Harvey <kusabi.software@gmail.com>
2019-07-11 21:56:29 +0100

    Added a PSR-7 stream wrapper

---
+++ src/Stream.php
@@ -0,0 +1,256 @@
+<?php
+
+namespace Kusabi\Stream;
+
+use Exception;
+use Psr\Http\Message\StreamInterface;
+use RuntimeException;
+
+class Stream implements StreamInterface
+{
+    /**
+     * The underlying resource we are wrapped around
+     *
+     * @var resource
+     */
+    protected $resource;
+
+    /**
+     * Stream constructor.
+     *
+     * @param resource $resource
+     */
+    public function __construct($resource)
+    {
+        $this->resource = $resource;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see StreamInterface::__toString()
+     */
+    public function __toString()
+    {
+        try {
+            $this->rewind();
+            return $this->getContents();
+        } catch (Exception $exception) {
+            return '';
+        }
+    }
+
+    /**
+     * Get the resource this instance is wrapped around
+     *
+     * @return resource
+     */
+    public function getResource()
+    {
+        return $this->resource;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see StreamInterface::close()
+     */
+    public function close()
+    {
+        fclose($this->resource);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see StreamInterface::detach()
+     */
+    public function detach()
+    {
+        $resource = $this->resource;
+        $this->resource = null;
+        return $resource;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see StreamInterface::getSize()
+     */
+    public function getSize()
+    {
+        return $this->getStat('size');
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see StreamInterface::tell()
+     */
+    public function tell()
+    {
+        $result = ftell($this->resource);
+        if ($result !== false) {
+            return $result;
+        }
+        // @codeCoverageIgnoreStart
+        throw new RuntimeException('Could not determine the position on the stream');
+        // @codeCoverageIgnoreEnd
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see StreamInterface::eof()
+     */
+    public function eof()
+    {
+        return feof($this->resource);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see StreamInterface::isSeekable()
+     */
+    public function isSeekable()
+    {
+        return $this->getMetadata('seekable') === true;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see StreamInterface::seek()
+     */
+    public function seek($offset, $whence = SEEK_SET)
+    {
+        if (!$this->isSeekable()) {
+            throw new RuntimeException('Resource is not seekable');
+        }
+        $result = fseek($this->resource, $offset, $whence);
+        if ($result === -1) {
+            // @codeCoverageIgnoreStart
+            throw new RuntimeException('An unknown error occurred while trying to seek the resource');
+            // @codeCoverageIgnoreEnd
+        }
+        return $result;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see StreamInterface::rewind()
+     */
+    public function rewind()
+    {
+        $this->seek(0);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see StreamInterface::isWritable()
+     */
+    public function isWritable()
+    {
+        return in_array($this->getMetadata('mode'), ['r+', 'w', 'w+', 'a', 'a+', 'x', 'x+', 'c', 'c+', 'r+b', 'wb', 'w+b', 'ab', 'a+b', 'xb', 'x+b', 'cb', 'c+b', 'r+t', 'wt', 'w+t', 'at', 'a+t', 'xt', 'x+t', 'ct', 'c+t']);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see StreamInterface::write()
+     */
+    public function write($string)
+    {
+        if (!$this->isWritable()) {
+            throw new RuntimeException('Resource is not writable');
+        }
+        $bytes = fwrite($this->resource, $string);
+        if ($bytes === false) {
+            // @codeCoverageIgnoreStart
+            throw new RuntimeException('An unknown error occurred while trying to write to the resource');
+            // @codeCoverageIgnoreEnd
+        }
+        return $bytes;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see StreamInterface::isReadable()
+     */
+    public function isReadable()
+    {
+        return in_array($this->getMetadata('mode'), ['r', 'r+', 'w+', 'a+', 'x+', 'c+', 'rb', 'r+b', 'w+b', 'a+b', 'x+b', 'c+b', 'rt', 'r+t', 'w+t', 'a+t', 'x+t', 'c+t']);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see StreamInterface::read()
+     */
+    public function read($length)
+    {
+        if (!$this->isReadable()) {
+            throw new RuntimeException('Resource is not readable');
+        }
+        return fread($this->resource, $length) ?? '';
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see StreamInterface::getContents()
+     */
+    public function getContents()
+    {
+        if (!$this->isReadable()) {
+            throw new RuntimeException('Resource is not readable');
+        }
+        $result = stream_get_contents($this->resource);
+        if ($result === false) {
+            // @codeCoverageIgnoreStart
+            throw new RuntimeException('An unknown error occurred while trying to read the resource');
+            // @codeCoverageIgnoreEnd
+        }
+        return $result;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see StreamInterface::getMetadata()
+     */
+    public function getMetadata($key = null)
+    {
+        $metadata = stream_get_meta_data($this->resource);
+        if ($key === null) {
+            return $metadata;
+        }
+        return $metadata[$key] ?? null;
+    }
+
+    /**
+     * Get stream stats as an associative array or retrieve a specific key.
+     *
+     * The keys returned are identical to the keys returned from PHPs fstat() function.
+     *
+     * @link https://php.net/manual/en/function.fstat.php
+     *
+     * @param string $key Specific stat to retrieve.
+     *
+     * @return array|mixed|null Returns an associative array if no key is
+     *     provided. Returns a specific key value if a key is provided and the
+     *     value is found, or null if the key is not found.
+     */
+    public function getStat($key = null)
+    {
+        $stats = fstat($this->resource);
+        if ($key === null) {
+            return $stats;
+        }
+        return $stats[$key] ?? null;
+    }
+}
---
+++ tests/StreamTest.php
@@ -0,0 +1,327 @@
+<?php
+
+namespace Tests;
+
+use Kusabi\Stream\Stream;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+class StreamTest extends TestCase
+{
+    public function testCreateStreamAndGetResource()
+    {
+        $resource = fopen('php://temp', 'r+');
+        $stream = new Stream($resource);
+        $this->assertSame($resource, $stream->getResource());
+    }
+
+    public function testToStringReturnsWholeValue()
+    {
+        $text = 'this is some test data';
+        $resource = fopen('php://temp', 'r+');
+        fwrite($resource, $text);
+        $stream = new Stream($resource);
+        $this->assertSame($text, (string) $stream);
+    }
+
+    public function testToStringReturnsEmptyStringWhenNotReadable()
+    {
+        $resource = fopen('php://stdin', 'w');
+        fwrite($resource, '.');
+        $stream = new Stream($resource);
+        $this->assertSame('', (string) $stream);
+    }
+
+    public function testCloseClosesTheResource()
+    {
+        $resource = fopen('php://temp', 'r+');
+        $stream = new Stream($resource);
+        $stream->close();
+        $this->assertNotEquals('stream', get_resource_type($resource));
+    }
+
+    public function testDetachReturnsAndRemovesTheResource()
+    {
+        $resource = fopen('php://temp', 'r+');
+        $stream = new Stream($resource);
+        $this->assertSame($resource, $stream->detach());
+        $this->assertNull($stream->getResource());
+        $this->assertNull($stream->detach());
+    }
+
+    public function testGetSizeReturnsSize()
+    {
+        $resource = fopen('php://temp', 'r+');
+        fwrite($resource, 'this is some test data');
+        rewind($resource);
+        $stream = new Stream($resource);
+        $this->assertSame(22, $stream->getSize());
+    }
+
+    public function testTellGivesCorrectPosition()
+    {
+        $resource = fopen('php://temp', 'r+');
+        fwrite($resource, 'this is some test data');
+        rewind($resource);
+        $stream = new Stream($resource);
+        $this->assertSame(0, $stream->tell());
+        fseek($resource, 5);
+        $this->assertSame(5, $stream->tell());
+    }
+
+    public function testEofKnowsIfWeHaveReachedTheEndOfTheStream()
+    {
+        $resource = fopen('php://temp', 'r+');
+        fwrite($resource, 'this is some test data');
+        rewind($resource);
+        $stream = new Stream($resource);
+        while (!feof($resource)) {
+            $this->assertFalse($stream->eof());
+            fread($resource, 5);
+        }
+        $this->assertTrue($stream->eof());
+    }
+
+    public function testIsSeekableIsCorrect()
+    {
+        $stream = new Stream(fopen('php://temp', 'r+'));
+        $this->assertTrue($stream->isSeekable());
+
+        $stream = new Stream(fopen('php://stdout', 'r+'));
+        $this->assertFalse($stream->isSeekable());
+    }
+
+    public function testSeekWorksAsExpected()
+    {
+        $text = 'this is some test data';
+        $resource = fopen('php://temp', 'r+');
+        fwrite($resource, $text);
+        rewind($resource);
+        $stream = new Stream($resource);
+
+        $stream->seek(1);
+        $this->assertSame(1, $stream->tell());
+
+        $stream->seek(1, SEEK_CUR);
+        $this->assertSame(2, $stream->tell());
+
+        $stream->seek(1, SEEK_SET);
+        $this->assertSame(1, $stream->tell());
+
+        $stream->seek(0, SEEK_END);
+        $this->assertSame(strlen($text), $stream->tell());
+    }
+
+    public function testSeekThrowsExceptionIfNotSeekable()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Resource is not seekable');
+        $stream = new Stream(fopen('php://stdout', 'r+'));
+        $stream->seek(1);
+    }
+
+    public function testRewindWorks()
+    {
+        $text = 'this is some test data';
+        $resource = fopen('php://temp', 'r+');
+        fwrite($resource, $text);
+        rewind($resource);
+        $stream = new Stream($resource);
+
+        $stream->seek(10);
+        $this->assertSame(10, $stream->tell());
+        $stream->rewind();
+        $this->assertSame(0, $stream->tell());
+    }
+
+    public function testRewindThrowsExceptionIfNotSeekable()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Resource is not seekable');
+        $stream = new Stream(fopen('php://stdout', 'r+'));
+        $stream->rewind();
+    }
+
+    public function providesReadableWritableData()
+    {
+        return [
+            ['r', true, false],
+            ['r+', true, true],
+            ['w', false, true],
+            ['w+', true, true],
+            ['a', false, true],
+            ['a+', true, true],
+            ['x', false, true],
+            ['x+', true, true],
+            ['c', false, true],
+            ['c+', true, true],
+        ];
+    }
+
+    /**
+     * @param string $mode
+     * @param bool $isReadable
+     * @param bool $isWritable
+     *
+     * @dataProvider providesReadableWritableData
+     */
+    public function testReadableAndWritable($mode, $isReadable, $isWritable)
+    {
+        $stream = new Stream(fopen('php://stdin', $mode));
+        $this->assertSame($isReadable, $stream->isReadable());
+        $this->assertSame($isWritable, $stream->isWritable());
+    }
+
+    public function testWriteWorksCorrectly()
+    {
+        $resource = fopen('php://temp', 'r+');
+        fwrite($resource, 'hello ');
+        $stream = new Stream($resource);
+        $this->assertSame(6, ftell($resource));
+        $stream->write('6 more');
+        $this->assertSame(12, ftell($resource));
+        rewind($resource);
+        $this->assertSame('hello 6 more', stream_get_contents($resource));
+    }
+
+    public function testWriteThrowsExceptionIfNotWritable()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Resource is not writable');
+        $stream = new Stream(fopen('php://stdin', 'r'));
+        $stream->write('test');
+    }
+
+    public function testReadWorksCorrectly()
+    {
+        $resource = fopen('php://temp', 'r+');
+        fwrite($resource, 'hello ');
+        rewind($resource);
+        $stream = new Stream($resource);
+        $this->assertSame('hel', $stream->read(3));
+        $this->assertSame('lo', $stream->read(2));
+        $this->assertSame(' ', $stream->read(1));
+    }
+
+    public function testReadThrowsExceptionIfNotReadable()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Resource is not readable');
+        $stream = new Stream(fopen('php://stdin', 'w'));
+        $stream->read(1);
+    }
+
+    public function testGetContentsWorksCorrectly()
+    {
+        $resource = fopen('php://temp', 'r+');
+        fwrite($resource, 'hello');
+        rewind($resource);
+        $stream = new Stream($resource);
+        $this->assertSame('hello', $stream->getContents());
+        fseek($resource, 2);
+        $this->assertSame('llo', $stream->getContents());
+    }
+
+    public function testGetContentsThrowsExceptionWhenNotReadable()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Resource is not readable');
+        $resource = fopen('php://stdout', 'w');
+        $stream = new Stream($resource);
+        $stream->getContents();
+    }
+
+    public function testGetMetaDataWithoutKeyReturnsAll()
+    {
+        $resource = fopen('php://temp', 'r+');
+        $stream = new Stream($resource);
+        $this->assertSame([
+            'wrapper_type' => 'PHP',
+            'stream_type' => 'TEMP',
+            'mode' => 'w+b',
+            'unread_bytes' => 0,
+            'seekable' => true,
+            'uri' => 'php://temp'
+        ], $stream->getMetadata());
+    }
+
+    public function testGetMetaDataWithKeyReturnsValueOrNull()
+    {
+        $resource = fopen('php://temp', 'r+');
+        $stream = new Stream($resource);
+        $this->assertSame('PHP', $stream->getMetadata('wrapper_type'));
+        $this->assertSame('TEMP', $stream->getMetadata('stream_type'));
+        $this->assertSame('w+b', $stream->getMetadata('mode'));
+        $this->assertSame(0, $stream->getMetadata('unread_bytes'));
+        $this->assertSame(true, $stream->getMetadata('seekable'));
+        $this->assertSame('php://temp', $stream->getMetadata('uri'));
+        $this->assertNull($stream->getMetadata('not-real'));
+    }
+
+    public function testGetStatsWithoutKeyReturnsAll()
+    {
+        $resource = fopen('php://temp', 'r+');
+        $stream = new Stream($resource);
+        $this->assertSame([
+            0 => 12,
+            1 => 0,
+            2 => 33206,
+            3 => 1,
+            4 => 0,
+            5 => 0,
+            6 => -1,
+            7 => 0,
+            8 => 0,
+            9 => 0,
+            10 => 0,
+            11 => -1,
+            12 => -1,
+            'dev' => 12,
+            'ino' => 0,
+            'mode' => 33206,
+            'nlink' => 1,
+            'uid' => 0,
+            'gid' => 0,
+            'rdev' => -1,
+            'size' => 0,
+            'atime' => 0,
+            'mtime' => 0,
+            'ctime' => 0,
+            'blksize' => -1,
+            'blocks' => -1
+        ], $stream->getStat());
+    }
+
+    public function testGetStatWithKeyReturnsValueOrNull()
+    {
+        $resource = fopen('php://temp', 'r+');
+        $stream = new Stream($resource);
+        $this->assertSame(12, $stream->getStat(0));
+        $this->assertSame(0, $stream->getStat(1));
+        $this->assertSame(33206, $stream->getStat(2));
+        $this->assertSame(1, $stream->getStat(3));
+        $this->assertSame(0, $stream->getStat(4));
+        $this->assertSame(0, $stream->getStat(5));
+        $this->assertSame(-1, $stream->getStat(6));
+        $this->assertSame(0, $stream->getStat(7));
+        $this->assertSame(0, $stream->getStat(8));
+        $this->assertSame(0, $stream->getStat(9));
+        $this->assertSame(0, $stream->getStat(10));
+        $this->assertSame(-1, $stream->getStat(11));
+        $this->assertSame(-1, $stream->getStat(12));
+        $this->assertSame(12, $stream->getStat('dev'));
+        $this->assertSame(0, $stream->getStat('ino'));
+        $this->assertSame(33206, $stream->getStat('mode'));
+        $this->assertSame(1, $stream->getStat('nlink'));
+        $this->assertSame(0, $stream->getStat('uid'));
+        $this->assertSame(0, $stream->getStat('gid'));
+        $this->assertSame(-1, $stream->getStat('rdev'));
+        $this->assertSame(0, $stream->getStat('size'));
+        $this->assertSame(0, $stream->getStat('atime'));
+        $this->assertSame(0, $stream->getStat('mtime'));
+        $this->assertSame(0, $stream->getStat('ctime'));
+        $this->assertSame(-1, $stream->getStat('blksize'));
+        $this->assertSame(-1, $stream->getStat('blocks'));
+        $this->assertNull($stream->getStat('not-real'));
+    }
+}
